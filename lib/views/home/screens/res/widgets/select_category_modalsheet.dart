508c8d631d42af239f4401a811d8e611d70de67a
joe ward <joewrrd@joes-MacBook-Air.local>
2025-05-02 13:23:36 +0300

    Day 7

--- lib/views/home/screens/res/widgets/select_category_modalsheet.dart
+++ lib/views/home/screens/res/widgets/select_category_modalsheet.dart
@@ -1,23 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:plainliving/utils/constants/colors.dart';
 import 'package:plainliving/utils/constants/sizes.dart';
+import 'package:plainliving/utils/helpers/helper.dart';
 
 class SelectCategoryModalSheet extends StatelessWidget {
   const SelectCategoryModalSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = HelperFunctions.isDarkMode(context);
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.92,
+      height: MediaQuery.of(context).size.height * 0.7,
       child: Padding(
         padding: EdgeInsets.only(
-          top: ConstantSizes.defaultSpace / 2,
+          top: ConstantSizes.defaultSpace / 2.5,
           left: 6,
           right: 6,
           bottom: ConstantSizes.defaultSpace,
         ),
-        child: Column(children: [
-            
-          ],  
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () => Get.back(),
+                  icon: Icon(
+                    FontAwesomeIcons.circleXmark,
+                    size: 21,
+                    color: isDark ? ConstantColors.white : ConstantColors.black,
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 40),
+                      child: Text(
+                        'Select Category',
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color:
+                              isDark
+                                  ? ConstantColors.white
+                                  : ConstantColors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(
+                      FontAwesomeIcons.car,
+                      size: 21,
+                      color:
+                          isDark ? ConstantColors.white : ConstantColors.black,
+                    ),
+                    title: Text(
+                      "Transportation",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color:
+                            isDark
+                                ? ConstantColors.white
+                                : ConstantColors.black,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
